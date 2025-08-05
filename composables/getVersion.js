ddd2a335671c088887ff638b49a49d7a1be5324a
Rana-Hassan7272 <ssc.shahbaz.2004@gmail.com>
2025-08-05 21:48:59 +0500

    Update: made changes

--- composables/getVersion.js
+++ composables/getVersion.js
@@ -1,17 +1,41 @@
 export default function () {
   return () => {
-    // Only call useRoute when we're in a proper context
+    if (process.env.NODE_ENV === 'production') {
+      return 'published';
+    }
+
+    // Local dev / preview
     if (process.client) {
       const route = useRoute();
       if (route.query._storyblok_published !== undefined) {
         return 'published';
-      }
-      else {
+      } else {
         return 'draft';
       }
     }
-    
-    // During SSR or when not in client context, return draft version
+
+    // SSR fallback
     return 'draft';
   };
 }
+
+
+
+
+//export default function () {
+  //return () => {
+    // Only call useRoute when we're in a proper context
+    //if (process.client) {
+      //const route = useRoute();
+      //if (route.query._storyblok_published !== undefined) {
+      //  return 'published';
+    //  }
+      //else {
+        //return 'draft';
+     // }
+    //}
+    
+    // During SSR or when not in client context, return draft version
+    //return 'draft';
+  //}//;
+//}
